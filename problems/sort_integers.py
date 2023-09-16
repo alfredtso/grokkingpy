a17651e6d325617a97882f1d13ac5d87dba81a57
alfredtso <32275603+alfredtso@users.noreply.github.com>
2023-09-16 05:57:53 +0000

    sort integer by something

---
+++ problems/sort_integers.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def sort_by_bits(self, arr: List[int]) -> List[int]:
+        """
+        >>> arr = [1024,512,256,128,64,32,16,8,4,2,1]
+        """
+        print("blah")
+
+if __name__ == '__main__':
+    arr = [1024,512,256,128,64,32,16,8,4,2,1]
+    sol = Solution()
+    sol.sort_by_bits(arr)
