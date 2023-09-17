86044343b31f41af9179f53f6e40d0468b54fcdf
alfredtso <32275603+alfredtso@users.noreply.github.com>
2023-09-17 08:31:38 +0000

    1356

--- problems/sort_integers.py
+++ problems/sort_integers.py
@@ -1,13 +1,19 @@
 from typing import List
+from collections import defaultdict
 
 class Solution:
     def sort_by_bits(self, arr: List[int]) -> List[int]:
         """
-        >>> arr = [1024,512,256,128,64,32,16,8,4,2,1]
+        >>> arr= [1024,512,256,128,64,32,16,8,4,2,1]
+        >>> arr= [1,2,4,8,16,32,64,128,256,512,1024]
         """
-        print("blah")
+        # sort by number of 1 and length in binary rep
+        arr =sorted(arr, key=lambda x: (x.bit_count(), x))
+        print(arr)
 
 if __name__ == '__main__':
     arr = [1024,512,256,128,64,32,16,8,4,2,1]
     sol = Solution()
     sol.sort_by_bits(arr)
+    import doctest
+    doctest.testmod()
